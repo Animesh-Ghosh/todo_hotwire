7c996cec8bcac7455922337aab5a879ce78afa02
Animesh-Ghosh <animesh.ghosh@tuta.io>
2024-07-03 23:56:23 +0530

    feat: Add realtime broadcasts for Tasks update
    It's a two-line change, lol

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :completed, inclusion: { in: [true, false] }
+
+  broadcasts_to ->(task) { :tasks }, inserts_by: :prepend
 end
